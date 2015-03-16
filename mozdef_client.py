64074a3613425b5e4aeb6b3fd3f8276aebf771d0
Aaron Meihm <ameihm@mozilla.com>
2015-03-16 11:00:06 -0500

    validate dict in validate_log() vs object (for compliance msg)

--- mozdef_client.py
+++ mozdef_client.py
@@ -296,18 +296,21 @@ class MozDefAssetHint(MozDefEvent):
 
 class MozDefCompliance(MozDefEvent):
     def validate_log(self):
+        if 'details' not in self._sendlog:
+            return False
+        t = self._sendlog['details']
         for k in ['target', 'policy', 'check', 'compliance', 'link',
             'utctimestamp']:
-            if k not in self.details.keys():
+            if k not in t.keys():
                 return False
         for k in ['level', 'name', 'url']:
-            if k not in self.details['policy'].keys():
+            if k not in t['policy'].keys():
                 return False
         for k in ['description', 'location', 'name', 'test']:
-            if k not in self.details['check'].keys():
+            if k not in t['check'].keys():
                 return False
         for k in ['type', 'value']:
-            if k not in self.details['check']['test'].keys():
+            if k not in t['check']['test'].keys():
                 return False
         return True
 
