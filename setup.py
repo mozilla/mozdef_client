bf19db51eba439f513d7f4cf2387b7361cfc3d90
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2017-02-17 12:24:01 -0800

    bump 1.0.10

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 setup(
     name = "mozdef_client",
         py_modules = ['mozdef_client'],
-        version = "1.0.9",
+        version = "1.0.10",
         author = "Guillaume Destuynder",
         author_email = "gdestuynder@mozilla.com",
         description = ("A client library to send messages/events using MozDef"),
