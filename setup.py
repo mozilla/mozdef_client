1e97931cce7ed1227681bc0fbe32c06941f6951e
Greg Cox <gcox@mozilla.com>
2018-12-12 23:12:35 +0000

    pep8 and URL cleanup against setup.py

--- setup.py
+++ setup.py
@@ -6,27 +6,28 @@
 # Author: gdestuynder@mozilla.com
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "mozdef_client",
-        py_modules = ['mozdef_client'],
-        version = "1.0.11",
-        author = "Guillaume Destuynder",
-        author_email = "gdestuynder@mozilla.com",
-        description = ("A client library to send messages/events using MozDef"),
-        license = "MPL",
-        keywords = "mozdef client library",
-        url = "https://github.com/gdestuynder/mozdef_client",
-        long_description = read('README.rst'),
-        install_requires = ['requests_futures', 'pytz', 'boto3'],
-        classifiers = [
-            "Development Status :: 5 - Production/Stable",
-            "Topic :: System :: Logging",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        ],
+    name="mozdef_client",
+    py_modules=['mozdef_client'],
+    version="1.0.11",
+    author="Guillaume Destuynder",
+    author_email="gdestuynder@mozilla.com",
+    description=("A client library to send messages/events using MozDef"),
+    license="MPL",
+    keywords="mozdef client library",
+    url="https://github.com/mozilla/mozdef_client",
+    long_description=read('README.rst'),
+    install_requires=['requests_futures', 'pytz', 'boto3'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: System :: Logging",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    ],
 )
